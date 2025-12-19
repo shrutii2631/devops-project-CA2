cab9d85d1153f8daff292a0343b6132bcc0aa60c
shruti <skummari63160@gmail.com>
2025-12-19 19:27:49 +0530

    first commit

--- Scripts/test.sh
+++ Scripts/test.sh
@@ -1,1 +1,15 @@
-echo "Hello DevOps"
+#!/bin/bash
+
+echo "Generating DevOps Report..."
+
+echo "PROJECT: Git-based DevOps Workflow" > ../reports/summary.txt
+echo "-------------------------------" >> ../reports/summary.txt
+
+echo "Git Status:" >> ../reports/summary.txt
+git status >> ../reports/summary.txt
+
+echo "" >> ../reports/summary.txt
+echo "Recent Commits:" >> ../reports/summary.txt
+git log --oneline >> ../reports/summary.txt
+
+echo "Report Generated Successfully"
